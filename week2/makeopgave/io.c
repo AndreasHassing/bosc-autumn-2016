bbebbb3362ac0a2198534ffecfb954ecaf71e92d
AndreasHassing <andreas@famhassing.dk>
2016-09-06 11:00:12 +0200

    Slight alterations to io.c

--- week2/makeopgave/io.c
+++ week2/makeopgave/io.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "io.h"
 
-void
-io(struct Person p[], int num_people) {
+void io(struct Person p[], int num_people) {
     int i;
     for (i = 0; i < num_people; i++) {
-        printf("Person age: %d and gender: %c\n", p[i].age, p[i].gender);
+        printf("Person %d, age: %d and gender: %c\n", i, p[i].age, p[i].gender);
     }
 };
