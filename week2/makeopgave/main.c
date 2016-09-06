96b26026a078096f715de40b083008822bfe1763
AndreasHassing <andreas@famhassing.dk>
2016-09-06 11:00:55 +0200

    Sort people array in main

--- week2/makeopgave/main.c
+++ week2/makeopgave/main.c
@@ -8,6 +8,7 @@ main ( int argc, char *argv[] )   {
 	printf("Starting my job...\n");
     struct Person people[5];
     data(people, 5);
+    qsort(people, 5, sizeof(struct Person), comp);
 	io(people, 5);
 	printf("Done with my job.\n");
         return(EXIT_SUCCESS);
